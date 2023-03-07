321e5da0d265891469d65c375848606bc6d78855
Rodrigo <rodrigo.olmos@aol.com>
2023-03-06 19:02:30 -0800

    started models

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Technician(models.Model):
+    name = models.CharField(max_length=100)
+    employee_number = models.CharField(max_length=100)
+
+
+class ServiceAppointment(models.Model):
+    vin = models.CharField(max_length=16)
+    date = models.DateField()
+    reason = models.CharField(max_length=300)
+    #customer foreign
+    #technician foreign
