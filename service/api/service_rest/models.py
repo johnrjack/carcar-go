a6cbb7135b0fdf9dd28f81b58c12b5403d4ab4bb
Rodrigo <rodrigo.olmos@aol.com>
2023-03-07 11:29:33 -0800

    add more models

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -3,14 +3,31 @@ from django.db import models
 # Create your models here.
 
 
+class AutomobileVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True, null=True)
+    color = models.CharField(max_length=50)
+    year = models.PositiveSmallIntegerField()
+    vin = models.CharField(max_length=17, unique=True)
+
+    def __str__(self):
+        return self.model
+
 class Technician(models.Model):
     name = models.CharField(max_length=100)
     employee_number = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class ServiceAppointment(models.Model):
     vin = models.CharField(max_length=16)
     date = models.DateField()
     reason = models.CharField(max_length=300)
+    technician = models.ForeignKey(
+        Technician,
+        related_name="tech",
+        on_delete=models.PROTECT,
+    )
     #customer foreign
     #technician foreign
