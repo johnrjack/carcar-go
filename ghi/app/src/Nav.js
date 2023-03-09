a2ba80af222c7ee18b71260533423557d33f5944
John Jackson <jonathrjacks@gmail.com>
2023-03-08 21:54:31 -0800

    updated Navbar

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -8,20 +8,37 @@ function Nav() {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className='nav-item'>
-              <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav  mb-2 mb-lg-0">
+            <li className='nav-item dropdown'>
+              <a className='nav-link dropdown-toggle' data-bs-toggle="dropdown" aria-haspopup='true' aria-expanded='false'>Inventory</a>
+              <ul className='dropdown-menu' aria-labelledby='navbarDropdownMenuLink'>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/manufacturer/">Manufacturers</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/models/">Vehicle Models</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/automobiles/">Automobiles</NavLink></li>
+              </ul>
             </li>
-            <li className='nav-item'>
-              <NavLink className="nav-link" aria-current="page" to="/automobiles/">Automobiles</NavLink>
-              <NavLink className="nav-link" aria-current="page" to="/manufacturer/">Manufacturers</NavLink>
-              <NavLink className="nav-link" aria-current="page" to="/models/">Vehicle Models</NavLink>
-              <NavLink className="nav-link" aria-current="page" to="/sales-team/">Add Sales Team Member</NavLink>
-              <NavLink className="nav-link" aria-current="page" to="/customer/">Create Potential Customer</NavLink>
-              <NavLink className="nav-link" aria-current="page" to="/sales-record/">Create a Sales Record</NavLink>
-              <NavLink className="nav-link" aria-current="page" to="/sales-list-all/">List of All Sales</NavLink>
-              <NavLink className="nav-link" aria-current="page" to="/sales-by-person/">Sale Person History</NavLink>
+          </ul>
+          <ul className="navbar-nav  mb-2 mb-lg-0">
+            <li className='nav-item dropdown'>
+              <a className='nav-link dropdown-toggle' data-bs-toggle="dropdown" aria-haspopup='true' aria-expanded='false'>Sales</a>
+              <ul className='dropdown-menu' aria-labelledby='navbarDropdownMenuLink'>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/sales-team/">Add Sales Team Member</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/customer/">Create Potential Customer</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/sales-record/">Create a Sales Record</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/sales-list-all/">List of All Sales</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/sales-by-person/">Sale Person History</NavLink></li>
+              </ul>
+            </li>
+          </ul>
+          <ul className="navbar-nav  mb-2 mb-lg-0">
+            <li className='nav-item dropdown'>
+              <a className='nav-link dropdown-toggle' data-bs-toggle="dropdown" aria-haspopup='true' aria-expanded='false'>Service</a>
+              <ul className='dropdown-menu' aria-labelledby='navbarDropdownMenuLink'>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
             </li>
           </ul>
         </div>
@@ -31,3 +48,5 @@ function Nav() {
 }
 
 export default Nav;
+
+
